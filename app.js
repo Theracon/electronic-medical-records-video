1735e0f1f842e4e977516ea9302a9fa61a892645
Theracon <54535899+Theracon@users.noreply.github.com>
2021-07-24 12:49:23 +0100

    Add frontend heroku url to cors origin

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://e-medrecords.herokuapp.com" || "http://localhost:3000",
+    origin: "https://emedrecords-prod.herokuapp.com" || "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
